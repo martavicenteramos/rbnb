5ddde9134dc3fece0d63fc7ea365198dda0af506
Vytautas Leoncikas <v.leo@protonmail.com>
2019-11-18 17:59:36 +0000

    Improved dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,5 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
 
   def index
     @dogs = Dog.all
@@ -14,5 +15,11 @@ class DogsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
   end
 end
