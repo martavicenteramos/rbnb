77461d7d451ebd2596589ca0157bd8af2f0559ba
Vytautas Leoncikas <v.leo@protonmail.com>
2019-11-19 11:59:54 +0000

    Changed User controller to Users and added path to redirect after login to dogs show and personal users page in the navbar)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    # return the path based on resource
+    request.referrer
+  end
+
+
 end
