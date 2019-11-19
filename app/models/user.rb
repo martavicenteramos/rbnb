6d2817be6d5a3021fb462f320799bebed13f63ea
rafael soares <soares.pratas.r@gmail.com>
2019-11-19 12:24:26 +0000

    Working on Review method to verify one per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :dogs
+  has_many :reviews, inverse_of: :user
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
