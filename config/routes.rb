509302db4193938a54d1e891aba36b6561b679bc
Vytautas Leoncikas <v.leo@protonmail.com>
2019-11-20 11:08:14 +0000

    Doesnt ask to sign in on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :dogs do
     resources :bookings
     resources :pictures, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :update, :edit]
 end
 
