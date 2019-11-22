41a0d10b91ed3ef95a8fbbf5cb0d26b950ce4a58
Marta Ramos <martavmr26297@gmail.com>
2019-11-22 11:33:18 +0000

    Map is responsive to the dogs in show

--- app/javascript/components/init_mapbox.js
+++ app/javascript/components/init_mapbox.js
@@ -8,12 +8,6 @@ const initMapbox = () => {
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
-  const fitMapToMarkersSingle = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    bounds.extend([ markers.lng, markers.lat ]);
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
-
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -22,21 +16,13 @@ const initMapbox = () => {
       style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    if (Array.isArray(markers)) {
       markers.forEach((marker) => {
         new mapboxgl.Marker()
           .setLngLat([ marker.lng, marker.lat ])
           .addTo(map);
       });
       fitMapToMarkers(map, markers);
-    }  else {
-        new mapboxgl.Marker()
-          .setLngLat([ markers.lng, markers.lat ])
-          .addTo(map);
-
-      fitMapToMarkersSingle(map, markers);
     }
-  }
 };
 
 export { initMapbox };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,29 +3,32 @@ import { toggleDateInputs } from '../components/datepicker.js';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../components/init_mapbox';
+import {initAutocomplete } from '../components/init_autocomplete';
 
 toggleDateInputs();
 initMapbox();
+initAutocomplete();
 
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js';
 import { searchBox, hits } from 'instantsearch.js/es/widgets';
 
-// const searchClient = algoliasearch('6NT8IPD8TK', '5d00eaa8d0f473939c0735eb49bdf538');
 
-// const search = instantsearch({
-//   indexName: 'Dog',
-//   searchClient,
-// });
+const searchClient = algoliasearch('6NT8IPD8TK', '5d00eaa8d0f473939c0735eb49bdf538');
 
-// search.addWidgets([
-//   searchBox({
-//     container: "#searchbox"
-//   }),
+const search = instantsearch({
+  indexName: 'Dog',
+  searchClient,
+});
 
-//   hits({
-//     container: "#hits"
-//   })
-// ]);
+search.addWidgets([
+  searchBox({
+    container: "#searchbox"
+  }),
 
-// search.start();
+  hits({
+    container: "#hits"
+  })
+]);
+
+search.start();
