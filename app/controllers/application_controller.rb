20c19cd9a40aeb7cc57b8ba6ce36f09874cc02b7
Jaqueline Pinto <jaque86@gmail.com>
2020-02-11 14:23:18 +0000

    Dash com timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # return the path based on resource
-    user_path(current_user.id)
+    startups_path
   end
 
   def after_sign_out_path_for(resource)
     # return the path based on resource
-    request.referrer
+    startups_path
   end
 
   protected
